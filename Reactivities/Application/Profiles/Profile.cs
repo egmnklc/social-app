ba2e6d832d8889f8c1e9293e433d64d6e036462a
Egemen <developer.egemen@gmail.com>
2023-10-08 13:50:06 +0300

    Following/Follower backend deployed

--- Reactivities/Application/Profiles/Profile.cs
+++ Reactivities/Application/Profiles/Profile.cs
@@ -4,10 +4,15 @@ namespace Application.Profiles
 {
     public class Profile
     {
-     public string Username { get; set; }   
-     public string DisplayName { get; set; }   
-     public string Bio { get; set; }   
-     public string Image { get; set; }   
-     public ICollection<Photo> Photos { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        //* Determine if the currently logged in user is following a currently viewed profile.
+        public bool Following { get; set; }
+        //* We'll use AutoMapper to map the results of these into the profile class.
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
